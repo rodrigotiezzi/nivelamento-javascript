0a6f2da27c30477ed332359c541b48b486206d51
rodrigotiezzi <rm562975@fiap.com>
2026-03-16 21:09:30 -0300

    exercicio-1-1 - Print do meu nome, curso e que estou aprendendo JavaScript.

---
+++ exercicio-1-1.js
@@ -0,0 +1,8 @@
+//codigo de exemplo
+console.log("Olá, mundo! Este é meu primeiro script.");
+console.log("Aprendendo JavaScript com React Native.");
+
+//Exercicio
+console.log("Rodrigo Tiezzi");
+console.log("Analise e desenvolvimento de sistemas");
+console.log("Estou aprendendo JavaScript!");
