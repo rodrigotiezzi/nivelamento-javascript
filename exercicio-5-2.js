8139cd7f0164f93cee299acdc1e27c92912013bc
rodrigotiezzi <rm562975@fiap.com.br>
2026-03-18 20:18:44 -0300

    Exercicio 5 - IF e Switch

---
+++ exercicio-5-2.js
@@ -0,0 +1,31 @@
+let diaSemana = 3;
+
+switch (diaSemana) {
+    case 1:
+        console.log("Segunda-Feira");
+        break;
+
+    case 2:
+        console.log("Terça-Feira");
+        break;
+
+    case 3:
+        console.log("Quarta-Feira");
+        break;
+
+    case 4:
+        console.log("Quinta-Feira");
+        break;
+
+    case 5:
+        console.log("Sexta-Feira");
+        break;
+
+    case 6:
+        console.log("Sabado");
+        break;
+
+    case 7:
+        console.log("Domingo");
+        break;
+}
