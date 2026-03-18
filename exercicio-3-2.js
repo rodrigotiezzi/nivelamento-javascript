95291dccb16d5a279e2d4f94b884ebf937b18365
rodrigotiezzi <rm562975@fiap.com.br>
2026-03-18 19:33:53 -0300

    Exercicio 3.2 - Tipos Primitivos: null e undefined

---
+++ exercicio-3-2.js
@@ -0,0 +1,7 @@
+let aluno = { nome: "Humberto da Silva", matricula: 3 };
+console.log(aluno.nome);        
+console.log(aluno.matricula);       
+console.log(aluno.nota);    
+// Adicionando a propriedade depois:
+aluno.nota = 8;
+console.log(aluno.nota);   
