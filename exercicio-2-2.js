3b57f16020f85db2830ddd460767a2cd243702a2
rodrigotiezzi <rm562975@fiap.com.br>
2026-03-18 19:48:53 -0300

    Alteração do nome do exercicio

---
+++ exercicio-2-2.js
@@ -0,0 +1,8 @@
+let nome = "Rodrigo Tiezzi";
+let idade = 19;
+const ativa = true;
+let vazio;
+console.log(typeof nome);    // "string"
+console.log(typeof idade);  // "number"
+console.log(typeof ativa);  // "boolean"
+console.log(typeof vazio); //"undefined"
