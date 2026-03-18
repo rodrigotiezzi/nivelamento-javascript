8f8774e84e110dd417f054a5047ed1f0ed50354c
rodrigotiezzi <rm562975@fiap.com.br>
2026-03-18 19:33:38 -0300

    Exercicio 3.1 - Tipos Primitivos: null e undefined

---
+++ exercicio-3-1.js
@@ -0,0 +1,10 @@
+let enderecoEntrega = null;         
+let numeroPedido;              
+console.log(enderecoEntrega);       // null
+console.log(numeroPedido);       // undefined
+
+// Simulando entrega:
+enderecoEntrega = "Playstation 5";
+numeroPedido = "23";
+console.log(enderecoEntrega );       
+console.log(numeroPedido );   
