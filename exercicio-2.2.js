64fab98220266b5406f1e19ec061d3c4b25b6840
rodrigotiezzi <rm562975@fiap.com.br>
2026-03-18 19:51:21 -0300

    Exercicio 4.1 - Exibindo propridades com [ ] e .

--- exercicio-2.2.js
+++
@@ -1,8 +0,0 @@
-let nome = "Rodrigo Tiezzi";
-let idade = 19;
-const ativa = true;
-let vazio;
-console.log(typeof nome);    // "string"
-console.log(typeof idade);  // "number"
-console.log(typeof ativa);  // "boolean"
-console.log(typeof vazio); //"undefined"
---
+++ exercicio-4-1.js
@@ -0,0 +1,6 @@
+let tarefas = ["Arrumar a cama", "Escovar os dentes", "Tomar banho"];
+console.log(tarefas[0]);          
+console.log(tarefas[2]);          
+console.log(tarefas.length);      
+tarefas.push("Tomar café da manhã");            
+console.log(tarefas.length);      
