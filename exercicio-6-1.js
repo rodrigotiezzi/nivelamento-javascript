fcedc5f36f879a48e5d18dfa6a3f26897b9581bc
rodrigotiezzi <rm562975@fiap.com.br>
2026-03-18 20:32:06 -0300

    Exercicio 6 - For e While

---
+++ exercicio-6-1.js
@@ -0,0 +1,11 @@
+
+for (let i = 0; i <= 10; i++) {
+    
+    let alunos = ["Ana", "Bruno", "Clara", "Diego"];
+    v_par = i % 2;
+
+  if(v_par === 0){   
+  console.log("Contador:", i);
+  console.log(alunos[i]);
+  }
+}
---
+++ exercicio-6-2.js
@@ -0,0 +1,5 @@
+let contagem = 10;
+while (contagem >= 0) {
+  console.log("Número:", contagem);
+  contagem--
+}
